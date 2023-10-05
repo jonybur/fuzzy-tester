3383978218726846efd1164e132a1ea9991087f1
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-10-05 01:44:14 -0400

    Use BigInts, add further operations

--- fuzzer.test.js
+++ fuzzer.test.js
@@ -9,9 +9,10 @@ describe("Noir Fuzzy Test Runner", () => {
 
   beforeAll(() => {
     ensureDirectoryStructure();
+    const max64bit = 18446744073709551615n;
     for (let i = 0; i < 10; i++) {
-      const valueA = getRandomInt(1, 100000);
-      const valueB = getRandomInt(1, 100000);
+      const valueA = getRandomBigInt(max64bit / 3n, max64bit / 2n);
+      const valueB = getRandomBigInt(max64bit / 3n, max64bit / 2n);
       const result = valueA + valueB;
       valuesToTest.push({ valueA, valueB, result });
     }
@@ -26,7 +27,6 @@ describe("Noir Fuzzy Test Runner", () => {
 async function execTest() {
   return new Promise((resolve, reject) => {
     const command = spawn("nargo", ["test"], { cwd: distDir });
-
     command.on("close", (code) => {
       if (code === 0) {
         resolve();
@@ -34,7 +34,6 @@ async function execTest() {
         reject("Noir tests did not pass.");
       }
     });
-
     command.on("error", (error) => {
       reject(`Error executing nargo test: ${error}`);
     });
@@ -42,27 +41,73 @@ async function execTest() {
 }
 
 function generateNr(valuesToTest) {
+  const imports = `
+    use dep::std::option::Option;
+  `;
+
   const tests = valuesToTest.reduce((acc, item, index) => {
     const { valueA, valueB, result } = item;
     return (
       acc +
       `
       #[test]
-      fn test${index}() { assert(add(${valueA},${valueB}) == ${result}); }
+      fn test${index}() { 
+        let none = Option::none();
+        let some = Option::some(${valueA});
+      
+        assert(none.is_none());
+        assert(some.is_some());
+
+        assert(add(${valueA},${valueB}) == ${result});
+
+        assert(some.unwrap() == ${valueA});
+
+        assert(none.unwrap_or(${valueB}) == ${valueB});
+        assert(some.unwrap_or(${valueB}) == ${valueA});
+
+        assert(none.unwrap_or_else(|| ${valueB + result}) == ${
+        valueB + result
+      });
+        assert(some.unwrap_or_else(|| ${valueB + result}) == ${valueA});
+
+        assert(none.map(|x| x * ${valueB}).is_none());
+        assert(some.map(|x| x * ${valueB}).unwrap() == ${valueA * valueB});
+
+        assert(none.map_or(${valueB}, |x| x * ${valueB}) == ${valueB});
+        assert(some.map_or(${valueB}, |x| x * ${valueB}) == ${valueA * valueB});
+
+        assert(none.map_or_else(|| ${valueB}, |x| x * ${valueB}) == ${valueB});
+        assert(some.map_or_else(|| ${valueB}, |x| x * ${valueB}) == ${
+        valueA * valueB
+      });
+
+        assert(none.and(some).is_none());
+        assert(some.and(none).is_none());
+        assert(some.and(some).is_some());
+
+        assert(none.or(some).is_some());
+        assert(some.or(none).is_some());
+        assert(some.or(some).is_some());
+
+        assert(none.xor(some).is_some());
+        assert(some.xor(none).is_some());
+        assert(some.xor(some).is_none());
+      }
     `
     );
   }, "");
 
   const scriptContent = `
-  fn add(x: u64, y: u64) -> u64 { x + y }
-  ${tests}
+    ${imports}
+    fn add(x: u64, y: u64) -> u64 { x + y }
+    ${tests}
   `;
 
   const nargoToml = `[package]
-  name = "fuzzer"
-  type = "bin"
-  compiler_version = "0.10.5"
-  [dependencies]`;
+    name = "fuzzer"
+    type = "bin"
+    compiler_version = "0.10.5"
+    [dependencies]`;
 
   fs.writeFileSync(`${distDir}/Nargo.toml`, nargoToml);
   fs.writeFileSync(`${distDir}/src/main.nr`, scriptContent);
@@ -75,6 +120,6 @@ function ensureDirectoryStructure() {
   }
 }
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomBigInt(min, max) {
+  return min + BigInt(Math.floor(Math.random() * Number(max - min + 1n)));
 }
