5279e86cf139ff02d0b0bbf552e217eac45bc719
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-10-05 14:01:24 -0400

    Changes

--- fuzzer.test.js
+++ fuzzer.test.js
@@ -26,12 +26,22 @@ describe("Noir Fuzzy Test Runner", () => {
 
 async function execTest() {
   return new Promise((resolve, reject) => {
-    const command = spawn("nargo", ["test"], { cwd: distDir });
+    const command = spawn("nargo", ["prove"], { cwd: distDir });
     command.on("close", (code) => {
       if (code === 0) {
-        resolve();
+        const command = spawn("nargo", ["verify"], { cwd: distDir });
+        command.on("close", (code) => {
+          if (code === 0) {
+            resolve();
+          } else {
+            reject("Could not verify.");
+          }
+        });
+        command.on("error", (error) => {
+          reject(`Error executing nargo test: ${error}`);
+        });
       } else {
-        reject("Noir tests did not pass.");
+        reject("Could not prove.");
       }
     });
     command.on("error", (error) => {
@@ -41,76 +51,15 @@ async function execTest() {
 }
 
 function generateNr(valuesToTest) {
-  const imports = `
-    use dep::std::option::Option;
+  const test = valuesToTest[0];
+
+  const proverToml = `
+    x = "${test.valueA}"
+    y = "${test.valueB}"
+    result = "${test.result}"
   `;
 
-  const tests = valuesToTest.reduce((acc, item, index) => {
-    const { valueA, valueB, result } = item;
-    return (
-      acc +
-      `
-      #[test]
-      fn test${index}() { 
-        let none = Option::none();
-        let some = Option::some(${valueA});
-      
-        assert(none.is_none());
-        assert(some.is_some());
-
-        assert(add(${valueA},${valueB}) == ${result});
-
-        assert(some.unwrap() == ${valueA});
-
-        assert(none.unwrap_or(${valueB}) == ${valueB});
-        assert(some.unwrap_or(${valueB}) == ${valueA});
-
-        assert(none.unwrap_or_else(|| ${valueB + result}) == ${
-        valueB + result
-      });
-        assert(some.unwrap_or_else(|| ${valueB + result}) == ${valueA});
-
-        assert(none.map(|x| x * ${valueB}).is_none());
-        assert(some.map(|x| x * ${valueB}).unwrap() == ${valueA * valueB});
-
-        assert(none.map_or(${valueB}, |x| x * ${valueB}) == ${valueB});
-        assert(some.map_or(${valueB}, |x| x * ${valueB}) == ${valueA * valueB});
-
-        assert(none.map_or_else(|| ${valueB}, |x| x * ${valueB}) == ${valueB});
-        assert(some.map_or_else(|| ${valueB}, |x| x * ${valueB}) == ${
-        valueA * valueB
-      });
-
-        assert(none.and(some).is_none());
-        assert(some.and(none).is_none());
-        assert(some.and(some).is_some());
-
-        assert(none.or(some).is_some());
-        assert(some.or(none).is_some());
-        assert(some.or(some).is_some());
-
-        assert(none.xor(some).is_some());
-        assert(some.xor(none).is_some());
-        assert(some.xor(some).is_none());
-      }
-    `
-    );
-  }, "");
-
-  const scriptContent = `
-    ${imports}
-    fn add(x: u64, y: u64) -> u64 { x + y }
-    ${tests}
-  `;
-
-  const nargoToml = `[package]
-    name = "fuzzer"
-    type = "bin"
-    compiler_version = "0.10.5"
-    [dependencies]`;
-
-  fs.writeFileSync(`${distDir}/Nargo.toml`, nargoToml);
-  fs.writeFileSync(`${distDir}/src/main.nr`, scriptContent);
+  fs.writeFileSync(`${distDir}/Prover.toml`, proverToml);
 }
 
 function ensureDirectoryStructure() {
