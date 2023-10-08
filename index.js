04ac69a725d9d40331561d3d2d784ac6060e8c16
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-10-08 18:30:25 -0400

    Output results to file

--- index.js
+++ index.js
@@ -3,158 +3,178 @@ const { spawn } = require("child_process");
 const path = require("path");
 const fs = require("fs");
 
-const scriptsDir = path.join(__dirname, "scripts");
+const SCRIPTS_DIR = path.join(__dirname, "scripts");
 const MAX_64_BIT = 18446744073709551615n;
-const packages = [
+const PACKAGES = [
   { name: "sum", generator: generateSum },
-  { name: "subtract", generator: generateSubtract },
   { name: "multiply", generator: generateMultiply },
-  { name: "xor", generator: generateXor },
   { name: "and", generator: generateAnd },
+  { name: "right_shift", generator: generateRightShift },
   { name: "or", generator: generateOr },
+  { name: "subtract", generator: generateSubtract },
+  { name: "xor", generator: generateXor },
   { name: "not_equals", generator: generateNotEquals },
   { name: "equals", generator: generateEquals },
   { name: "greater_than", generator: generateGreaterThan },
   { name: "greater_than_or_equal", generator: generateGreaterThanOrEqual },
   { name: "less_than_or_equal", generator: generateLessThanOrEqual },
   { name: "less_than", generator: generateLessThan },
-  { name: "right_shift", generator: generateRightShift },
-  /////////////// NOT WORKING
+  // NOT WORKING:
   // { name: "left_shift", generator: generateLeftShift },
   // { name: "divide", generator: generateDivide },
   // { name: "bitwise_not", generator: generateBitwiseNot },
 ];
 
 async function main() {
+  let errors = [];
+
   while (true) {
     ensureDirectoryStructure();
 
-    // generate baseline & provers
-    await Promise.all(packages.map((package) => package.generator()));
+    let baselineResults;
 
-    try {
-      // run tests
-      await Promise.all(
-        packages.map(async (package) => {
-          const result = await execTest(package.name);
-          console.log(result);
-        })
-      );
-    } catch (e) {
-      console.error(e);
-    }
+    // generate baseline & provers
+    baselineResults = PACKAGES.map((package) => package.generator());
+
+    // run tests
+    await Promise.all(
+      PACKAGES.map(async (package, index) => {
+        const packageName = package.name;
+        const baselineResult = baselineResults[index].toString();
+        try {
+          const testOutput = await execTest(packageName);
+          if (testOutput.includes("Circuit witness successfully solved")) {
+            console.log(`${packageName} success`);
+          } else {
+            errors.push({
+              packageName,
+              testOutput,
+              baselineResult,
+            });
+          }
+        } catch (error) {
+          errors.push({
+            packageName,
+            error,
+            baselineResult,
+          });
+        }
+      })
+    );
+
+    fs.writeFileSync(`${SCRIPTS_DIR}/results.json`, JSON.stringify(errors));
   }
 }
 
 function generateSum() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = x + y;
-  generateProver("sum", { x, y, result });
+  generateProver("sum", { x, y });
+  return x + y;
 }
 
 function generateSubtract() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = x - y;
-  generateProver("subtract", { x, y, result });
+  generateProver("subtract", { x, y });
+  return x - y;
 }
 
 function generateMultiply() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = x * y;
-  generateProver("multiply", { x, y, result });
+  generateProver("multiply", { x, y });
+  return x * y;
 }
 
 function generateDivide() {
   const x = getRandomBigInt(MAX_64_BIT / 15n, MAX_64_BIT / 10n);
   const y = getRandomBigInt(MAX_64_BIT / 15n, MAX_64_BIT / 10n);
-  const result = x / y;
-  generateProver("divide", { x, y, result });
+  generateProver("divide", { x, y });
+  return x / y;
 }
 
 function generateXor() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = x ^ y;
-  generateProver("xor", { x, y, result });
+  generateProver("xor", { x, y });
+  return x ^ y;
 }
 
 function generateAnd() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = x & y;
-  generateProver("and", { x, y, result });
+  generateProver("and", { x, y });
+  return x & y;
 }
 
 function generateOr() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = x | y;
-  generateProver("or", { x, y, result });
+  generateProver("or", { x, y });
+  return x | y;
 }
 
 function generateLeftShift() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = BigInt(Math.floor(Math.random() * 64));
-  const result = x << y;
-  generateProver("left_shift", { x, y, result });
+  generateProver("left_shift", { x, y });
+  return x << y;
 }
 
 function generateRightShift() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = BigInt(Math.floor(Math.random() * 64));
-  const result = x >> y;
-  generateProver("right_shift", { x, y, result });
+  generateProver("right_shift", { x, y });
+  return x >> y;
 }
 
 function generateBitwiseNot() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = ~x;
-  generateProver("bitwise_not", { x, result });
+  generateProver("bitwise_not", { x });
+  return ~x;
 }
 
 function generateLessThan() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = x < y;
-  generateProver("less_than", { x, y, result });
+  generateProver("less_than", { x, y });
+  return x < y;
 }
 
 function generateLessThanOrEqual() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = x <= y;
-  generateProver("less_than_or_equal", { x, y, result });
+  generateProver("less_than_or_equal", { x, y });
+  return x <= y;
 }
 
 function generateGreaterThan() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = x > y;
-  generateProver("greater_than", { x, y, result });
+  generateProver("greater_than", { x, y });
+  return x > y;
 }
 
 function generateGreaterThanOrEqual() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = x >= y;
-  generateProver("greater_than_or_equal", { x, y, result });
+  generateProver("greater_than_or_equal", { x, y });
+  return x >= y;
 }
 
 function generateEquals() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = x == y;
-  generateProver("equals", { x, y, result });
+  generateProver("equals", { x, y });
+  return x == y;
 }
 
 function generateNotEquals() {
   const x = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
   const y = getRandomBigInt(MAX_64_BIT / 3n, MAX_64_BIT / 2n);
-  const result = x != y;
-  generateProver("not_equals", { x, y, result });
+  generateProver("not_equals", { x, y });
+  return x != y;
 }
 
 function isNumber(value) {
@@ -167,15 +187,15 @@ function generateProver(projectName, values) {
     return acc + `${name}=${isNumber(value) ? `"${value}"` : `${value}`}\n`;
   }, "");
   fs.writeFileSync(
-    `${scriptsDir}/crates/${projectName}/Prover.toml`,
+    `${SCRIPTS_DIR}/crates/${projectName}/Prover.toml`,
     proverToml
   );
 }
 
 function ensureDirectoryStructure() {
-  if (!fs.existsSync(scriptsDir)) {
-    fs.mkdirSync(scriptsDir);
-    fs.mkdirSync(`${scriptsDir}/src`);
+  if (!fs.existsSync(SCRIPTS_DIR)) {
+    fs.mkdirSync(SCRIPTS_DIR);
+    fs.mkdirSync(`${SCRIPTS_DIR}/src`);
   }
 }
 
@@ -183,7 +203,7 @@ async function execTest(package) {
   return new Promise((resolve, reject) => {
     let output = "";
     const command = spawn("nargo", ["execute", `--package`, package], {
-      cwd: scriptsDir,
+      cwd: SCRIPTS_DIR,
     });
 
     command.stdout.on("data", (data) => {
